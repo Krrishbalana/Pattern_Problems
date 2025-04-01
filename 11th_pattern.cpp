1dd8af9418200cb4d51976459c5443575ee5796b
Krrishbalana <krrishbalana@gmail.com>
2025-04-01 16:55:18 +0530

    modify 11th pattern

--- 11th_pattern.cpp
+++ 11th_pattern.cpp
@@ -6,15 +6,13 @@
 using namespace std;
 int main(){
     int n = 4;
-    int a = 1;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i-1; j++){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < i; j++){
             cout << " ";
         }
-        for(int k = 1; k <= 5-i; k++){
-            cout << a ;
+        for(int k = 1; k <= n-i; k++){
+            cout << (i+1) ;
         }
         cout << endl;
-        a++;
     }
 }
